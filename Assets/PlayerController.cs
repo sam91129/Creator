13615fe99047b91f6b80825932ad32a734349587
sam91129 <g00991188@gmail.com>
2023-08-03 20:14:22 +0800

    基礎移動系統全更新
    搖桿鍵盤對應操按鍵
    改為newinputsystem

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,31 +3,35 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{   
+{
     //移動所需變數
-    public CharacterController controller;
+    [Header("移動設置")]
+    Rigidbody _playerRig;
     public float walk;
     public float run;
-    public float gravity = -9.81f;//重力
-    Vector3 velocity;
+    float speed;
+    public float jump;
+    Vector2 moveValue;
+    Vector3 move;
 
     //滑鼠鎖定%控制所需變數
-    [Header("Control Setting")]
-    float mouseSensitivity = 100.0f;     //攝像機上下選轉X變化量
+    [Header("鏡頭轉動設置")]
+    public float mouseSensitivity;     //攝像機上下選轉X變化量
     float xRotation = 0f;
+    Vector2 mouseValue;
     public Transform playerbody;
     public Camera _camera;
 
     //地面確認所需變數
-    public Transform groundCheck;
-    public float grounDistance = 0.4f;
-    public LayerMask groundMask;
-    bool isGrounded;
+    [Header("地面檢測")]
+    public SphereCollider _groundCheck;
+    static public bool isGrounded;
 
     //視線檢測所需變數
-    [Header("Interaction Setting")]
+    [Header("準心設置")]
     [SerializeField] private float _maxDistance = 10.0f;
     [SerializeField] float _vistionRadius = 0.1f;
     [SerializeField] LayerMask _layerMask;
@@ -38,45 +42,31 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        _playerRig = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;   //上下不超過90度 
+        speed = walk;
     }
-
     void Update()
     {
-        //Movement(); //移動
-        GroundCheck();
-        Eye();      //第一人稱
-        ViweCast();
-
-        //Debug.Log(_hits);
-    }
-    void Movement()    {
-        float x = Input.GetAxis("Horizontal");  //平行
-        float z = Input.GetAxis("Vertical");    //垂直
-        Vector3 move = transform.right * x + transform.forward * z;
-        if (Input.GetKey(KeyCode.LeftShift)) controller.Move(move * run * Time.deltaTime);
-        else controller.Move(move * walk * Time.deltaTime);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-    }
-    void Eye()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; ;  //繞X軸旋轉
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; ;  //繞Y軸旋轉
-        xRotation -= mouseY;                                                            
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                                  //頭部轉90度
-        _camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);          //
-        playerbody.Rotate(Vector3.up * mouseX);
-    }
-    void GroundCheck()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, grounDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
+        if (moveValue != Vector2.zero)
         {
-            velocity.y = -2f;
+            this.transform.position += move * speed * Time.deltaTime;
         }
+        if (mouseValue != Vector2.zero)
+        {
+            float mouseX = mouseValue.x * mouseSensitivity * Time.deltaTime; ;  //繞X軸旋轉
+            float mouseY = mouseValue.y * mouseSensitivity * Time.deltaTime; ;  //繞Y軸旋轉
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);                                  //頭部轉90度
+            _camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);          //
+            playerbody.Rotate(Vector3.up * mouseX);
+        }
+        ViweCast();
     }
-    
+    public void onVision(InputAction.CallbackContext ctx)
+    {
+        mouseValue = ctx.ReadValue<Vector2>();
+    }
     void ViweCast()
     {
         _origin = _camera.transform.position;
@@ -87,7 +77,20 @@ public class PlayerController : MonoBehaviour
             _gameObject = _hits.collider.gameObject;
         }
     }
-
+    public void onMove(InputAction.CallbackContext ctx)    
+    {
+            moveValue = ctx.ReadValue<Vector2>();
+            move = transform.right * moveValue.x + transform.forward * moveValue.y;
+    }
+    public void onRun(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) speed = run;
+        if (ctx.canceled) speed = walk;
+    }
+    public void onJump(InputAction.CallbackContext ctx)
+    {
+        if(isGrounded) if (ctx.started) _playerRig.AddForce(0, jump*100.0f , 0);
+    }
     void UseItem()
     {
         //if (_gameObject != null && C)
