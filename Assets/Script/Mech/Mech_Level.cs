84e94a00d1d50f924124a9ddb7513cd7b81a6fea
ssw885800 <72431142+ssw885800@users.noreply.github.com>
2024-05-22 16:58:41 +0800

    更新日誌
    1.完成關於介面文本與圖片
    2.關卡選擇介面文本完善
    已知問題:
    1.bulid出的版本部會紀錄最佳成績(關卡選擇的最佳通冠紀錄不更新)
    2.進到TYFP場景後不會跳轉回主畫面

--- Assets/Script/Mech/Mech_Level.cs
+++ Assets/Script/Mech/Mech_Level.cs
@@ -14,14 +14,14 @@ public class Mech_Level : MonoBehaviour
     public void L1()
     {
         sceneName = GetSceneNameByIndex(1);
-        _level.text = "L1";
+        _level.text = "¹êÅç«Ç";
         _timer.text = PlayerPrefs.GetString(sceneName + "Timer", "00:00:00");
         GameObject.Find("MenuObject").GetComponent<Mech_MenuManager>().ContinueScnen = 1;
     }
     public void L2()
     {
         sceneName = GetSceneNameByIndex(2);
-        _level.text = "L2";
+        _level.text = "·¥¦a";
         _timer.text = PlayerPrefs.GetString(sceneName + "Timer", "00:00:00");
         GameObject.Find("MenuObject").GetComponent<Mech_MenuManager>().ContinueScnen = 2;
     }
